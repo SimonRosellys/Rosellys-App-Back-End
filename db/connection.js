3f485e115b13449fbb9b4e03a7eadd970aa19cc6
Simon Rosellys <simonrosellys@aol.com>
2022-08-19 23:34:59 +0100

    Configure for Heroku

--- db/connection.js
+++ db/connection.js
@@ -5,8 +5,43 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error("PGDATABASE not set");
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
+
+const db = new Pool(config);
+
+module.exports = db;
+
+// const { Pool } = require("pg");
+// const ENV = process.env.NODE_ENV || "development";
+
+// const config =
+//   ENV === "production"
+//     ? {
+//         connectionString: process.env.DATABASE_URL,
+//         ssl: {
+//           rejectUnauthorized: false,
+//         },
+//       }
+//     : {};
+
+// require("dotenv").config({
+//   path: `${__dirname}/../.env.${ENV}`,
+// });
+
+// if (!process.env.PGDATABASE) {
+//   throw new Error("PGDATABASE not set");
+// }
+
+// module.exports = new Pool(config);
--- listen.js
+++ listen.js
@@ -1,6 +1,15 @@
 const app = require("./app");
 
-app.listen(9090, (err) => {
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, (err) => {
   if (err) throw err;
-  console.log(`Server is listening on port 9090...`);
+  console.log(`Listening on ${PORT}...`);
 });
+
+// const app = require("./app");
+
+// app.listen(9090, (err) => {
+//   if (err) throw err;
+//   console.log(`Server is listening on port 9090...`);
+// });
